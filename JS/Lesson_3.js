f34d839713762cca3a97c5875dde525b022fb76d
MeetVasiyani <meetvasiyani.work@gmail.com>
2025-11-11 09:12:31 +0530

    Javascript Lesson 3

---
+++ JS/Lesson_3.js
@@ -0,0 +1,105 @@
+//operator
+
+// console.log(2+5)
+// console.log(2-5)
+// console.log(5/2)
+// console.log(5*2)
+// console.log(5%2)
+
+//Remainder
+// console.log(5**3)
+
+
+// let x = 20;
+// let y = 10;
+
+// x += y;
+// console.log(x)
+
+// x -= y;
+// console.log(x)
+
+// x *= y;
+// console.log(x)
+
+// let x = 20;
+// let y = 10;
+
+// console.log(x > y);
+// console.log(x <= y);
+// console.log(x >= y);
+// console.log(x > y);
+
+// z = "10";
+// let a = Number(z);
+// console.log(typeof a);
+
+// let c = 10
+// let y = "10"
+// console.log(c == y);
+// console.log(c === y);
+
+// let x = "12ac"
+// let y = Number(x);
+// console.log(y)
+
+// let b = 123
+// let d = String(b)
+// console.log(d)
+
+// console.log(Boolean(0));
+// console.log(Boolean(""))
+
+// let a = 0.1
+// let b = 0.2
+// let c = a+b
+// console.log(c == 0.3)
+// console.log(c)
+
+//null is loosely equal to undefined only
+// console.log(null == undefined);
+// console.log(null === undefined);
+// console.log(null == 0);
+
+// > < >= <= (Null --> Number)
+// console.log(null >= 0);
+
+// console.log("Rohit" > "Mohit")
+// console.log("Rohit" > "mohit")
+
+// console.log(NaN == NaN)
+
+// for(let i = 0; i < 10; i++){
+//     console.log(i);
+// }
+
+// let i = 0
+// while(i < 10){
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 0
+// do{
+//     console.log(i);
+//     i++;
+// }while(i < 10)
+
+// let age  = 25
+// if (age >= 18) {
+//     console.log("Eligible")
+// } else {
+//     console.log("No voting")
+// }
+
+let a = "Rohit"
+let b = "Mohit"
+
+let c = a&&b
+console.log(c)
+
+//&& : If first value is false it returns first value itself
+// If first value is true it will return second value
+
+//|| : If first value is true it returns first value itself
+// If first value is false it will return second value
