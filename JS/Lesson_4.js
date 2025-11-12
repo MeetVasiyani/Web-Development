91a255abaf1973a50515b27e106f562420ccddbd
MeetVasiyani <meetvasiyani.work@gmail.com>
2025-11-12 09:12:01 +0530

    Javascript Lesson  4

---
+++ JS/Lesson_4.js
@@ -0,0 +1,26 @@
+// let a = 123.45
+// console.log(a.toFixed(1))
+// console.log(typeof a.toFixed(1))
+
+// console.log(a.toPrecision(6));
+// console.log(a.toString());
+
+// let a = new Number(20);
+// let b = new Number(10);
+
+// console.log(a==b);
+
+// console.log(Boolean(new Number(0)))
+// console.log(Boolean(0))
+
+// console.log(Math.abs(-6))
+// console.log(Math.PI)
+// console.log(Math.LN10)
+// console.log(Math.SQRT2)
+// console.log(Math.ceil(1.23))
+// console.log(Math.floor(1.23))
+// console.log(Math.floor(Math.random()* 10))  // [0,1)
+// Math.floor(Math.random() * totalNoOfOutcomes) + shift)
+
+//11-20
+console.log(Math.floor(Math.random() * 10) + 11)
