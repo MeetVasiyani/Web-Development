4ca8053a0c73e4b7588b607be70d7b952bcf6011
MeetVasiyani <meetvasiyani.work@gmail.com>
2025-11-13 09:10:48 +0530

    Javascript Lesson 5

---
+++ JS/Lesson_5.js
@@ -0,0 +1,53 @@
+// let name = "Rohit";
+// let name1 = 'Rohit';
+// const day = 18
+// let name2 = `Rohit ${day}
+// python
+// javascript`;
+
+// console.log(name, name1, name2)
+
+const str = "Hello World"
+
+// console.log(str.length);
+// console.log(str[1]);
+// str[2]= 'W'
+// console.log(str);
+
+
+// console.log(str.toUpperCase())
+// console.log(str.indexOf('World'))
+// console.log(str.lastIndexOf('World'))
+// console.log(str.includes('World'))
+// console.log(str.slice(-5,-2))
+// console.log(str.substring(2,5))
+
+// const a = "King";
+// const b = "Cobra";
+// const c = a+" "+b;
+// console.log(c)
+
+// console.log(str.replace("Hello","Beautiful"));
+// console.log(str.replaceAll("o","Beautiful"));
+
+// const user = "   Hello World   "
+// console.log(user.trim())
+
+// const name = "A,B,C,D";
+// console.log(name.split(','))
+
+// const now = new Date();
+// console.log(now)
+// console.log(now.toLocaleString());
+
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getMonth());
+
+// const now = new Date(2025,8,20,8,25,16,125);
+// console.log(now.toString());
+
+// const now = Date.now();
+// console.log(now);
+// const date = new Date(now);
+// console.log(date);
